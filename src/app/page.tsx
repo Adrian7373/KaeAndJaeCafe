626913040fcfba24bf3ef638faf1cb3f545ffec5
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-06-19 16:41:36 +0800

    Refactor Home component layout and add hero and about sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,62 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="bg-kae-pink">
+      <header className="sticky">
+        <nav className="flex p-2 items-center justify-between bg-kae-light">
+          <div className="flex items-center gap-3">
+            <a href="#"><img className="w-15 h-15" src="logo.svg" alt="cafe logo" /></a>
+            <div className="flex flex-col font-pacifico">
+              <a className="text-kae-dark text-lg" href="#">Kae and</a>
+              <a className="text-kae-dark text-lg" href="#">Jae Cafe</a>
+            </div>
+
+          </div>
+          <div className="gap-3 hidden sm:block">
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Menu</a>
+            <a href="">Testimonials</a>
+            <a href="">Gallery</a>
+            <a href="">Contact</a>
+          </div>
+        </nav>
+      </header>
+
+      {/*Hero Section*/}
+      <section className="h-dvh">
+        <div>
+          <img src="hero-photo.svg" alt="food" />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col items-center text-center">
+            <h2 className="text-kae-light font-libertinus-serif text-4xl">Snacks.</h2>
+            <h2 className="text-kae-light font-libertinus-serif text-4xl">Sweets.</h2>
+            <h2 className="text-kae-light font-libertinus-serif text-4xl">Street food.</h2>
+          </div>
+          <p className="text-center text-kae-light px-2 font-roboto">Your go-to mobile spot for Korean-inspired bites, classic comfort food, and refreshing, sweet beverages. Catch us serving up your favorites daily.</p>
+          <div className="flex justify-center gap-5">
+            <button className="bg-kae-dark text-kae-light py-3 px-5 rounded-full font-roboto">Order Now</button>
+            <button className="border-1 py-3 px-5 rounded-full font-roboto">Contact Us</button>
+          </div>
         </div>
-      </main>
+      </section>
+
+      {/* About Us Section */}
+      <section>
+        <div className="bg-kae-light p-7">
+          <div className="flex flex-col items-center gap-6">
+            <h2 className="text-center">ABOUT US</h2>
+            <p className="text-center">What started as a love for good food turned into Camp Tinio's favorite mobile hangout. We realized that the best memories aren't made inside four walls, they happen wherever the food is hot and the drinks are sweet.</p>
+            <div>
+              <img className="rounded-full object-cover w-80 h-80" src="about-photo.jpg" alt="photo of customers" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Menu Section */}
     </div>
-  );
+  )
 }
