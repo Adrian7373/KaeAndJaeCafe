7bf7cbb21520c25177adf3aaec4ccb97591b4e34
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-06-19 17:00:52 +0800

    Enhance About Us and Menu sections with additional content and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
       <section>
         <div className="bg-kae-light p-7">
           <div className="flex flex-col items-center gap-6">
-            <h2 className="text-center">ABOUT US</h2>
+            <h2 className="text-center text-xl">ABOUT US</h2>
             <p className="text-center">What started as a love for good food turned into Camp Tinio's favorite mobile hangout. We realized that the best memories aren't made inside four walls, they happen wherever the food is hot and the drinks are sweet.</p>
             <div>
               <img className="rounded-full object-cover w-80 h-80" src="about-photo.jpg" alt="photo of customers" />
@@ -58,7 +58,55 @@ export default function Home() {
 
       {/* Menu Section */}
       <section>
-
+        <div className="flex flex-col items-center p-6 bg-kae-dark">
+          <h2 className="text-kae-light text-xl">OUR MENU</h2>
+          <div>
+            <div className="flex flex-col items-center">
+              <img className="w-60 h-60" src="chicken-meals.svg" alt="chicken meals" />
+              <p className="text-libertinus-serif text-kae-light text-lg font-medium">Chicken Meals</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <img className="w-60 h-60" src="waffles.svg" alt="waffles" />
+              <p className="text-libertinus-serif text-kae-light text-lg font-medium">Waffles</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <img className="w-60 h-60" src="bibimbap.svg" alt="bibimbap" />
+              <p className="text-libertinus-serif text-kae-light text-lg font-medium">Bibimbap</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <img className="w-60 h-60" src="korean-streetfoods.svg" alt="korean street foods" />
+              <p className="text-libertinus-serif text-kae-light text-lg font-medium">Korean Street Foods</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <img className="w-60 h-60" src="tofu.svg" alt="Tofu" />
+              <p className="text-libertinus-serif text-kae-light text-lg font-medium">Tofu</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <img className="w-60 h-60" src="rice-meals.svg" alt="rice meals" />
+              <p className="text-libertinus-serif text-kae-light text-lg font-medium">Rice Meals</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <img className="w-60 h-60" src="corndogs.svg" alt="corndogs" />
+              <p className="text-libertinus-serif text-kae-light text-lg font-medium">Corndogs</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <img className="w-60 h-60" src="spicy-noodles-and-ramen.svg" alt="Spicy Noodles and Ramen" />
+              <p className="text-libertinus-serif text-kae-light text-lg font-medium">Spicy Noodles and Ramen</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <img className="w-60 h-60" src="burgers-and-sandwiches.svg" alt="Burgers and Sandwiches" />
+              <p className="text-libertinus-serif text-kae-light text-lg font-medium">Burgers and Sandwiches</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <img className="w-60 h-60" src="fries-and-nachos.svg" alt="Fries and Nachos" />
+              <p className="text-libertinus-serif text-kae-light text-lg font-medium">Fries and Nachos</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <img className="w-60 h-60" src="various-beverages.svg" alt="Various Beverages" />
+              <p className="text-libertinus-serif text-kae-light text-lg font-medium">Various Beverages</p>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   )
