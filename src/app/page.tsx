4753ab5a1ea7ea379a975a4ca0a1c9686bc08a1c
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-06-20 15:20:51 +0800

    Update footer social media links to be clickable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -202,8 +202,8 @@ export default function Home() {
       <footer className="bg-kae-dark p-5 flex flex-col items-center gap-3">
         <h1 className="text-kae-light font-md font-inter">© 2026 Kae and Jae Cafe</h1>
         <div className="flex gap-2">
-          <FaFacebook className="w-8 h-8 text-kae-light" />
-          <FaFacebookMessenger className="w-8 h-8 text-kae-light" />
+          <a href="https://www.facebook.com/profile.php?id=61569261819780"><FaFacebook className="w-8 h-8 text-kae-light" /></a>
+          <a href="https://www.facebook.com/messages/t/1051738004696972"><FaFacebookMessenger className="w-8 h-8 text-kae-light" /></a>
         </div>
       </footer>
 
