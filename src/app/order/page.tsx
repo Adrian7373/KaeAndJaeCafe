fd619ebe95f523b4663c950cdab4ec56cbc35b48
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-06-29 23:41:59 +0800

    Refactor OrderPage to include CartProvider and improve error handling for menu items

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,22 +1,40 @@
-import OrderNavBar from "./_components/OrderNavBar"
+export const revalidate = 60;
 import MenuCatalog from "./_components/MenuCatalog"
 import { supabase } from "../../../lib/supabase"
+import OrderNavBar from "./_components/OrderNavBar";
+import { CartProvider } from "../../../context/CartContext";
 
 export default async function OrderPage() {
 
-    const { data: menuProducts, error: menuProductError } = await supabase
+    const { data: menuItems, error: menuItemsError } = await supabase
         .from("product")
         .select("id, name, image_path, price, discount_price, is_available, est_prep_time, category");
 
-    if (menuProductError) {
-        throw new Error(menuProductError.message)
+    if (menuItemsError) {
+        throw new Error(menuItemsError.message)
     }
+
+    const menuProducts = menuItems.map((item) => {
+        const { data: { publicUrl } } = supabase
+            .storage
+            .from("product_images")
+            .getPublicUrl(item.image_path);
+
+        return {
+            ...item,
+            imageUrl: publicUrl,
+        }
+    }) || [];
+
+    console.log(menuProducts);
 
     return (
         <>
-            <OrderNavBar />
-            <MenuCatalog products={menuProducts} />
-            <h1>HELLO CUSTOMER</h1>
+            <CartProvider>
+                <OrderNavBar />
+                <MenuCatalog products={menuProducts} />
+                <h1>HELLO CUSTOMER</h1>
+            </CartProvider>
         </>
     )
 }
