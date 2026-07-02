6ca45da826a2f71b8359a75fcdd63b8a51168ed8
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-07-02 16:53:44 +0800

    Fix order information retrieval and pass order status to OrderStatus component

--- src/app/track/[orderId]/page.tsx
+++ src/app/track/[orderId]/page.tsx
@@ -9,16 +9,29 @@ export default async function TrackPage({ params }: { params: { orderId: string 
         .from("order")
         .select("order_type, delivery_address, payment_method, status, first_name")
         .eq("id", orderId)
-        .maybeSingle;
+        .maybeSingle();
+
+    if (orderInfoError) {
+        throw new Error(`Failed to get order information: ${orderInfoError.message}`)
+    }
 
     const { data: orderDetails, error: orderDetailsError } = await supabase
         .from("order_items")
         .select("quantity, price_at_checkout, product(name)")
         .eq("order_id", orderId);
 
+    const orderStatus = {
+        orderType: orderInfo?.order_type,
+        delivery_address: orderInfo?.delivery_address,
+        payment_method: orderInfo?.payment_method,
+        status: orderInfo?.status,
+        first_name: orderInfo?.first_name,
+        orders: orderDetails
+    }
+
     return (
         <>
-            <OrderStatus />
+            <OrderStatus orderStatus={orderStatus} />
         </>
     )
 }
