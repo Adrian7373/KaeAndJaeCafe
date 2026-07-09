68cd6a7a4c1181012183615921d1bae4a9251f61
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-07-09 15:44:59 +0800

    Fixed footer: now postions at the bottom of the page even there is not enough content.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { FaFacebook, FaFacebookMessenger } from "react-icons/fa"
 
 export default function Footer() {
     return (
-        <footer className="bg-kae-dark p-5 flex flex-col items-center gap-3">
+        <footer className="bg-kae-dark p-5 flex flex-col items-center gap-3 mt-auto">
             <h1 className="text-kae-light font-md font-inter">© 2026 Kae and Jae Cafe</h1>
             <div className="flex gap-2">
                 <a href="https://www.facebook.com/profile.php?id=61569261819780"><FaFacebook className="w-8 h-8 text-kae-light" /></a>
