778dc8bfe271d5f0cd509512c3d7dc2e6b49402b
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-07-18 02:06:08 +0800

    Wrapped children inside AuthProvider

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,13 +1,20 @@
+import { getAuthenticatedUser, getCurrentUser } from "../actions";
 import AdminSidebar from "./_component/AdminSidebar";
+import { AuthProvider } from "../../../context/AuthContext";
 
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
+export default async function AdminLayout({ children }: { children: React.ReactNode }) {
+
+    const role = await getAuthenticatedUser()
+
     return (
-        <div className="flex h-screen bg-gray-50 overflow-hidden">
-            <AdminSidebar />
+        <AuthProvider role={role?.role}>
+            <div className="flex h-screen bg-gray-50 overflow-hidden">
+                <AdminSidebar role={role?.role} />
 
-            <main className="flex-1 overflow-y-auto">
-                {children}
-            </main>
-        </div>
+                <main className="flex-1 overflow-y-auto">
+                    {children}
+                </main>
+            </div>
+        </AuthProvider>
     );
 }
